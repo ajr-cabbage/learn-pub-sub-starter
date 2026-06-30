507547225b31b7cfc8ea2bab9dddf5bdc3aebbdd
ajr-cabbage <alexander.rakotz@gmail.com>
2026-06-29 23:34:30 -0400

    CH3:L3 Complete

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp091.Dial(connectionStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	fmt.Println("Connection Successful!")
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Running. Press Ctrl+C to Shut Down")
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// block until ctrl-c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down...")
 }
